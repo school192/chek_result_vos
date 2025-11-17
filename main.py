bf235fce9e32f30e7c2c4ec55ac1cc019f58d528
Oktopus90 <121638436+Oktopus90@users.noreply.github.com>
2025-11-17 14:02:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,40 +2,25 @@ import pyautogui
 import time
 import pyperclip
 
-olimp = '''sbec59/sch770192/9/r52294
-sbec59/sch770192/9/r528w4
-sbec59/sch770192/9/5r6q46
-sbec59/sch770192/9/r26qgr
-sbec59/sch770192/9/5rv2r2
-sbec59/sch770192/9/rw752r
-sbec59/sch770192/9/9rg3r7
-sbec59/sch770192/9/43vz6r
-sbec59/sch770192/9/z43v7r
-sbec59/sch770192/9/9rg6v4
-sbec59/sch770192/9/8rz8wr
-sbec59/sch770192/9/r2695r
-sbec59/sch770192/9/rq562r
-sbec59/sch770192/9/49qq94
-sbec59/sch770192/9/z43z7r
-sbec59/sch770192/9/rg96wr
-sbec59/sch770192/9/rv3qv4
-sbec59/sch770192/9/9rgw3r
-sbec59/sch770192/9/49q894
-sbec59/sch770192/9/q49v9r
-sbec59/sch770192/9/r52394
-sbec59/sch770192/9/r52gw4
-sbec59/sch770192/9/z4387r
-sbec59/sch770192/9/r6g6v4
-sbec59/sch770192/9/2r27q4
-sbec59/sch770192/9/rg9vwr
-sbec59/sch770192/9/wrqz74
-sbec59/sch770192/9/rv35v4
-sbec59/sch770192/9/8rz9w4
-sbec59/sch770192/9/748q2r
-sbec59/sch770192/9/rq582r
-sbec59/sch770192/9/r6g924
-sbec59/sch770192/9/r76v74
-sbec59/sch770192/9/rg9qwr
+olimp = '''
+sbip51/sch770192/10/z59v9z
+sbip51/sch770192/10/4w8q5z
+sbip51/sch770192/10/z2gvq4
+sbip51/sch770192/10/z2g554
+sbip51/sch770192/10/49wg54
+sbip51/sch770192/10/z7rq64
+sbip51/sch770192/10/4rq5rz
+sbip51/sch770192/10/z6w8v4
+sbip51/sch770192/10/zqv674
+sbip51/sch770192/10/z226gz
+sbip51/sch770192/10/z532rz
+sbip51/sch770192/10/z6wrv4
+sbip51/sch770192/10/4r9r5z
+sbip51/sch770192/10/z2vqgz
+sbip51/sch770192/10/4wr8qz
+sbip51/sch770192/10/z2v25z
+sbip51/sch770192/10/z7vq54
+sbip51/sch770192/10/48vv24
 '''
 
 list_olim = olimp.split()
@@ -46,13 +31,14 @@ EXIT_COORD = (1870, 140)
 FAMILE_COORD = (1780, 140)
 COMAND_COORD = (960, 50)
 
-def chek_olimp(user):
+
+def chek_olimp(user: str):
     pyautogui.click(INSERT_COORD)
     time.sleep(0.5)
     pyautogui.write(user)
     time.sleep(0.5)
     pyautogui.click(ENTER_COORD)
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.doubleClick(FAMILE_COORD)
     time.sleep(0.5)
     pyautogui.hotkey('ctrl', 'c')
@@ -70,4 +56,3 @@ for user in list_olim:
     print(user)
     time.sleep(1)
     chek_olimp(user)
-
