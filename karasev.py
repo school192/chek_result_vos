9e3c38425b398bc7b7c2dd89579145c8beb18c48
bonvech <bonvech@yandex.ru>
2025-11-21 15:02:37 +0300

    Update karasev.py -в

--- karasev.py
+++ karasev.py
@@ -3,11 +3,11 @@ import requests
 def get_results(token):
     r = requests.post("https://online.olimpiada.ru/smt-portal/register/login", json={"token":token}).json()
     if not "canPassTest" in r:
-        return ["Не учавствовал"]
+        return ["Не участвовал"]
     r = r["canPassTest"]
     r = requests.get(f"https://online.olimpiada.ru/smt-portal/test?regQuizId={r["liContestId"]}&sid={r["liSessionId"]}").json()
     t = list(filter(lambda t: t["tag"] == "HasResults", r["tsInfo"]["contents"]))
-    return r["tsUser"]["userName"], t[0]["contents"]["userScore"] if len(t) > 0 else "Не учавствовал"
+    return r["tsUser"]["userName"], t[0]["contents"]["userScore"] if len(t) > 0 else "Не участвовал"
 
 print("Введите коды, затем \"exec\"")
 lines = []
